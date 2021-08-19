128851830bd82f02bad7f7ae722ca3db081b340d
ReganOng <reganong0411@gmail.com>
2021-08-19 16:54:26 +0800

    Merge remote-tracking branch 'origin/main' into main

--- Group13_ASP_project.py
+++ Group13_ASP_project.py
@@ -3,19 +3,58 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = pd.read_excel("IMVA.xls", sheet_name="IMVA")
-newdata=data[['Periods','Americas', 'Canada', 'USA', 'Other Markets In Americas',
+df1=data[['Periods','Americas', 'Canada', 'USA', 'Other Markets In Americas',
              'Oceania', 'Australia', 'New Zealand', 'Other Markets In Oceania',
              'Africa', 'Egypt', 'Mauritius', 'South Africa (Rep Of)','Other Markets In Africa']]
-newdatas=newdata.replace(',','', regex=True)
+newdatas=df1.replace(',','', regex=True)
 newas=newdatas.replace('na','0',regex=True)
-print(newdata.index)
-print(newdata.columns)
+print(df1.index)
+print(df1.columns)
 print(newas.head(3))
 print(newas.tail(3))
-datas = newdata['Periods'].str.split(' ', n=1, expand=True)
-print(datas)
+countries = data[['Americas', 'Canada', 'USA', 'Other Markets In Americas',
+             'Oceania', 'Australia', 'New Zealand', 'Other Markets In Oceania',
+             'Africa', 'Egypt', 'Mauritius', 'South Africa (Rep Of)','Other Markets In Africa']]
+countries = countries.replace(',','', regex=True)
+countries = countries.replace('na', '0', regex=True)
+print(countries)
+countries3 = countries.astype(int)
+print(countries3.dtypes)
+psNotSorted = countries3.sum()
+print(psNotSorted)
+psSorted = psNotSorted.sort_values(ascending=False)
+print(psSorted)
 
+newdata = df1['Periods'].str.split(' ', n=1, expand=True)
+print(newdata)
+df2 = data.assign(Years=newdata[1])
+df2.index = df2["Years"]
+graph1=df2[(df2['Years']>=str(1998)) & (df2['Years']<=str(2007))]
 
+ps = psSorted.sort_values(ascending=False)
+index = np.arange(len(ps.index))
+plt.xlabel('Countries', fontsize=10)
+plt.ylabel('No. Of Travellers (in thousands)', fontsize=8)
+plt.xticks(index,ps.index,fontsize=8,rotation=90)
+plt.title('All Asia Countries from (Period:1998-2007)')
+plt.bar(ps.index,ps.values/1000)
+plt.show();
+
+top3=psSorted.head(3)
+print(top3)
+total=top3.values.sum()
+mean = round(top3.values.mean(),2)
+print("The total no. of visitors for the top 3 other country is ", total)
+print('The mean values of visitor for the top 3 other country is ' ,mean)
+
+ps=top3.sort_values(ascending=False)
+index = np.arange(len(ps.index))
+plt.xlabel('Countries', fontsize=10)
+plt.ylabel('No. Of Travellers (in thousands)',fontsize=8)
+plt.xticks(index,ps.index,fontsize=8,rotation=90)
+plt.title('Top 3 Asia countries from (Period:1998-2007')
+plt.bar(ps.index,ps.values/1000)
+plt.show();
 
 
 
