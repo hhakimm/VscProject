dfc489867253db8b77051245a5042b9f7981218a
ReganOng <reganong0411@gmail.com>
2021-08-19 15:04:13 +0800

    Merge remote-tracking branch 'origin/main' into main

--- Group13_ASP_project.py
+++ Group13_ASP_project.py
@@ -2,9 +2,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-excel = pd.read_excel("IMVA.xls", sheet_name="IMVA")
+data = pd.read_excel("IMVA.xls", sheet_name="IMVA")
+newdata=data[['Periods','Americas', 'Canada', 'USA', 'Other Markets In Americas',
+             'Oceania', 'Australia', 'New Zealand', 'Other Markets In Oceania',
+             'Africa', 'Egypt', 'Mauritius', 'South Africa (Rep Of)','Other Markets In Africa']]
+newdatas=newdata.replace(',','', regex=True)
+newas=newdatas.replace('na','0',regex=True)
+print(newdata.index)
+print(newdata.columns)
+print(newas.head(3))
+print(newas.tail(3))
+datas = newdata['Periods'].str.split(' ', n=1, expand=True)
+print(datas)
 
-countries = excel['Periods'].str.split(' ', n=1, expand=True)
-print(countries)
+
+
+
 
 
