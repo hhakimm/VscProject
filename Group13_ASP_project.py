1666706fad0c00ceb0106166ca71296a09ecd191
hhakimm <hadihakim00@icloud.com>
2021-08-19 10:18:34 +0800

    Merge remote-tracking branch 'origin/main'

--- Group13_ASP_project.py
+++ Group13_ASP_project.py
@@ -1,8 +1,3 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
 
-excel = pd.read_excel("IMVA.xls", sheet_name="IMVA")
 
-countries = excel ['Periods'].str.split(' ', n=1, expand=True)
-print(countries)
+
